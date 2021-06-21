c16bb59d4981d2036c78bf5c10f70ff4eac57b49
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2021-06-21 20:19:45 +0530

    Adding Live data to be updated from Broadcast Receiver

--- app/src/main/java/com/android/ranit/smartthermostat/model/DataManager.java
+++ app/src/main/java/com/android/ranit/smartthermostat/model/DataManager.java
@@ -1,5 +1,11 @@
 package com.android.ranit.smartthermostat.model;
 
+import android.bluetooth.BluetoothDevice;
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 /**
  * Created by: Ranit Raj Ganguly on 21/06/2021
  *
@@ -12,6 +18,8 @@ public class DataManager {
     // Private instance variable
     private static DataManager INSTANCE;
 
+    private MutableLiveData<BluetoothDevice> mBleDeviceMutableLiveData = new MutableLiveData<>();
+
     // Private Constructor
     private DataManager() {}
 
@@ -21,6 +29,32 @@ public class DataManager {
             INSTANCE = new DataManager();
         }
         return INSTANCE;
+    }
+
+    /**
+     * Updates the Mutable-Live-Data based upon the current Connection state
+     * received from ACL_Broadcast_Receiver
+     *
+     * @param bluetoothDevice - Bluetooth device object currently connected/disconnected
+     */
+    public void setBleDeviceLiveData(BluetoothDevice bluetoothDevice) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            // Main-Thread
+            mBleDeviceMutableLiveData.setValue(bluetoothDevice);
+        } else {
+            // Background-Thread
+            mBleDeviceMutableLiveData.postValue(bluetoothDevice);
+        }
+    }
+
+    /**
+     * Retrieves the Live-Data in order to be observed and perform necessary
+     * UI operations accordingly from View (MainActivity)
+     *
+     * @return mBleDeviceMutableLiveData - live data
+     */
+    public LiveData<BluetoothDevice> getBleDeviceLiveData() {
+        return mBleDeviceMutableLiveData;
     }
 
 }
